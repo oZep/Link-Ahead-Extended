a5e044e9e7ac99813e980a2cfe72b31ca5eba073
joeyissa <joeyissa59@gmail.com>
2024-03-02 08:03:58 -0500

    made links more green when it is badder or more red when it is badder

--- content.js
+++ content.js
@@ -3,9 +3,11 @@ linkText.forEach((link) => {
     // call api on link
     sketch_factor = fetch(`http://localhost:3000/api?url=${link.href}`)
     if (x >= 0.5) {
-        link.style.backgroundColor = 'red';
+        red = 255 * x
+        link.style.backgroundColor = 'rbg(${red}, 0, 0)';
     } else {
-        link.style.backgroundColor = 'green';
+        green = 255 * (1 - x)
+        link.style.backgroundColor = 'rbg(0, 0, ${green})';  
     }
 
 });
