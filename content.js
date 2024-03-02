ac035bb961785f48fb7e571ad1ecb0d8737349ea
joeyissa <joeyissa59@gmail.com>
2024-03-02 07:59:23 -0500

    base done

--- content.js
+++ content.js
@@ -1,8 +1,8 @@
 const linkText = document.querySelectorAll('a');
 linkText.forEach((link) => {
     // call api on link
-    x = fetch(`http://localhost:3000/api?url=${link.href}`)
-    if (x == false) {
+    sketch_factor = fetch(`http://localhost:3000/api?url=${link.href}`)
+    if (x >= 0.5) {
         link.style.backgroundColor = 'red';
     } else {
         link.style.backgroundColor = 'green';
