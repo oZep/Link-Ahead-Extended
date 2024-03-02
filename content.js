b9bcb638d895278fca225f9ee6b374ad240ccd15
joeyissa <joeyissa59@gmail.com>
2024-03-02 09:34:52 -0500

    added a boarder feature for images

--- content.js
+++ content.js
@@ -5,9 +5,11 @@ linkText.forEach((link) => {
     if (x >= 0.5) {
         var red = 255 * x
         link.style.backgroundColor = 'rbg(${red}, 0, 0)';
+        link.style.border = '3px solid rbg(${red}, 0, 0)';
     } else {
         var green = 255 * (1 - x)
         link.style.backgroundColor = 'rbg(0, 0, ${green})';  
+        link.style.border = '3px solid rbg(0, 0, ${green})';
     }
 
 });
