a2dc0cf396a26d50dcbb99bdc7a74cf52af21133
joeyissa <joeyissa59@gmail.com>
2024-03-02 08:08:11 -0500

    done

--- content.js
+++ content.js
@@ -3,10 +3,10 @@ linkText.forEach((link) => {
     // call api on link
     sketch_factor = fetch(`http://localhost:3000/api?url=${link.href}`)
     if (x >= 0.5) {
-        red = 255 * x
+        var red = 255 * x
         link.style.backgroundColor = 'rbg(${red}, 0, 0)';
     } else {
-        green = 255 * (1 - x)
+        var green = 255 * (1 - x)
         link.style.backgroundColor = 'rbg(0, 0, ${green})';  
     }
 
