168f968eea43c33244ae948405b1fffc25584ae7
joeyissa <joeyissa59@gmail.com>
2024-03-02 14:10:39 -0500

    this actually works

--- content.js
+++ content.js
@@ -4,13 +4,13 @@ linkText.forEach((link) => {
     var sketch_factor = link.href.length / 100;
     if (sketch_factor >= 0.5) {
         var red = 255 * sketch_factor
-        link.style.backgroundColor = 'rgb(${red}, 0, 0)';
-        link.style.border = '3px solid rgb(${red}, 0, 0)';
+        link.style.backgroundColor = `rgb(${red}, 0, 0)`;
+        link.style.border = `3px solid rgb(${red}, 0, 0)`;
         console.log('red');
     } else {
         var green = 255 * (1 - sketch_factor)
-        link.style.backgroundColor = 'rgb(0, ${green}, 0)';  
-        link.style.border = '3px solid rgb(0, ${green}, 0)';
+        link.style.backgroundColor = `rgb(0, ${green}, 0)`;  
+        link.style.border = `3px solid rgb(0, ${green}, 0)`;
     }
 
 });
