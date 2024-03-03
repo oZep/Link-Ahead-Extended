8836a3eb03c57aeb74812cd57c27a9b0e8caa743
joeyissa <joeyissa59@gmail.com>
2024-03-02 20:50:44 -0500

    only show bad links for images

--- content.js
+++ content.js
@@ -1,16 +1,19 @@
 const linkText = document.querySelectorAll('a');
 linkText.forEach((link) => {
+    const hasImage = link.querySelector('img') !== null;
     // call api on link
+
     var sketch_factor = link.href.length / 100;
     if (sketch_factor >= 0.5) {
         var red = 255 * sketch_factor
-        link.style.backgroundColor = `rgb(${red}, 0, 0)`;
-        link.style.border = `3px solid rgb(${red}, 0, 0)`;
+        link.style.color = `rgb(${red}, 0, 0)`;
+        if (hasImage) {
+            link.style.border = `3px solid rgb(${red}, 0, 0)`;
+        }
         console.log('red');
     } else {
         var green = 255 * (1 - sketch_factor)
-        link.style.backgroundColor = `rgb(0, ${green}, 0)`;  
-        link.style.border = `3px solid rgb(0, ${green}, 0)`;
+        link.style.color = `rgb(0, ${green}, 0)`;  
     }
 
 });
